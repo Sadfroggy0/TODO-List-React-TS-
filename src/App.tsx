88de6bc9f11174a2c2c1c1acb00979ddb87182c4
Timofey <tim.panov1337@gmail.com>
2022-11-16 00:02:28 +0300

    Test router success

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,58 @@
-import React, {useState} from 'react';
-import {TaskItem} from "./components/TaskItem"
-import {AddForm} from "./components/AddForm"
-import {ITask} from "./models/ITask";
+// import React, {useState} from 'react';
+// import {TaskItem} from "./components/TaskItem"
+// import {AddForm} from "./components/AddForm"
+// import {ITask} from "./models/ITask";
+import {Home}from './routes/Home'
+import {TestPage} from './routes/TestPage'
+import {Router, Route, Routes} from 'react-router-dom'
+
 
 function App() {
 
-    const [counter,setCounter] = useState(1) //id generator :)
+    // const [counter,setCounter] = useState(1) //id generator :)
 
-    const[items,setItems] = useState<ITask[]>([])
+    // const[items,setItems] = useState<ITask[]>([])
 
-    const addItem = (receivedItem:string)=>{
-        if (receivedItem !== ''){
-            const newItem:ITask = {id:counter, name: receivedItem}
-            setCounter(counter+1)
-            items.push(newItem)
-            setItems([...items])
-            console.log(items)
-        }
-    }
-    const deleteItem= (id:number) =>{
-        setItems([...items.filter((item)=>item.id !== id)])
-        // items.splice(id-1,1)
-        // setItems([...items])
-    }
+    // const addItem = (receivedItem:string)=>{
+    //     if (receivedItem !== ''){
+    //         const newItem:ITask = {id:counter, name: receivedItem}
+    //         setCounter(counter+1)
+    //         items.push(newItem)
+    //         setItems([...items])
+    //         console.log(items)
+    //     }
+    // }
+    // const deleteItem= (id:number) =>{
+    //     setItems([...items.filter((item)=>item.id !== id)])
+    //     // items.splice(id-1,1)
+    //     // setItems([...items])
+    // }
 
     return (
+        
+        <>
+            <Routes>
+                <Route path='/' element={<Home/>}></Route>
+                <Route path='/test' element= {<TestPage/>}/>
+            </Routes>
+        </>
+        
     
-        <div className="grid place-content-start md:place-content-center p-5">
-            <h1 className="grid place-content-start md:place-content-center">Counter: {items.length}</h1>
-            <AddForm  addItem = {addItem} />
+        // <div className="grid place-content-start md:place-content-center p-5">
+        //     <h1 className="grid place-content-start md:place-content-center">Counter: {items.length}</h1>
+        //     <AddForm  addItem = {addItem} />
 
-            { items.map((item) => {
-                return (
-                    <TaskItem
-                        item = {item}
-                        deleteItem = {deleteItem}
-                        key = {item.id}
+        //     { items.map((item) => {
+        //         return (
+        //             <TaskItem
+        //                 item = {item}
+        //                 deleteItem = {deleteItem}
+        //                 key = {item.id}
 
-                    ></TaskItem>
-                )
-            })}
-        </div>
+        //             ></TaskItem>
+        //         )
+        //     })}
+        // </div>
     )
 }
 
---
+++ src/routes/TestPage.tsx
@@ -0,0 +1,8 @@
+export const TestPage = () =>{
+
+    return(
+        <div>
+            <h1>THIS IS TEST PAGE</h1>
+        </div>
+    )
+}
